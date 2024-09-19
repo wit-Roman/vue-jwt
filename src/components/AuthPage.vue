34c4f2be1276ca7cc0d1708148ca427e7e9ee232
Роман Булычев <rbulychev@logrocon.local>
2024-09-19 18:00:51 +0300

    antd

--- src/components/AuthPage.vue
+++ src/components/AuthPage.vue
@@ -1,40 +1,70 @@
 <template>
 	<div>
 		<h2>Авторизация</h2>
-		<form @submit.prevent="handleLogin">
-			<div>
-				<label for="login">Login:</label>
-				<input id="login" v-model="login" type="text" required />
-			</div>
-			<div>
-				<label for="password">Passw:</label>
-				<input id="password" v-model="password" type="password" required />
-			</div>
-			<button type="submit">Enter</button>
-			<p v-if="error">{{ error }}</p>
-		</form>
+
+		<a-alert if="error">{{ error }}</a-alert>
+
+		<a-form
+			:model="formState"
+			name="authForm"
+			:label-col="{ span: 8 }"
+			:wrapper-col="{ span: 16 }"
+			autocomplete="off"
+			@finish="handleLogin"
+			@finishFailed="setError"
+		>
+			<a-form-item
+				label="Username"
+				name="username"
+				:rules="[{ required: true, message: 'Please input your username!' }]"
+			>
+				<a-input v-model:value="formState.username" />
+			</a-form-item>
+
+			<a-form-item
+				label="Password"
+				name="password"
+				:rules="[{ required: true, message: 'Please input your password!' }]"
+			>
+				<a-input-password v-model:value="formState.password" />
+			</a-form-item>
+
+			<a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+				<a-button type="primary" html-type="submit">Submit</a-button>
+			</a-form-item>
+		</a-form>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { useRouter } from "vue-router";
+
 import { useAuthStore } from "src/stores/auth";
+
+const error = ref("");
+const formState = reactive({
+	username: "",
+	password: "",
+});
 
 const authStore = useAuthStore();
 const router = useRouter();
-const login = ref("");
-const password = ref("");
-const error = ref("");
 
 const handleLogin = () => {
 	authStore
-		.login(login.value, password.value)
+		.login(formState.username, formState.password)
 		.then(() => {
 			router.push("/dashboard");
 		})
 		.catch(() => {
 			error.value = "Invalid credentials";
 		});
+};
+
+const setError = (errs: { errorFields: [] }) => {
+	if (errs.errorFields.length) {
+		error.value = "Bad value";
+	}
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import Antd from "ant-design-vue";
 
 import App from "./App.vue";
 import router from "./router";
@@ -7,6 +8,7 @@ import router from "./router";
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(Antd);
 app.use(pinia);
 app.use(router);
 //app.use(instance); если здесь подключать axios то max call stack
